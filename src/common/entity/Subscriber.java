9c83385eecd07f36a4da6392f08b39a12aadff1a
Zehavit <zehavito999@gmail.com>
2019-01-12 21:39:19 +0200

    we added update details by subscriber (without checking input)

--- src/common/entity/Subscriber.java
+++ src/common/entity/Subscriber.java
@@ -13,11 +13,12 @@ public class Subscriber extends User implements Serializable {
 //	Subscriber() {
 //		this.readerCard = new ReaderCard();
 //	}
-	public Subscriber(Integer usrId, String usrName, String usrPassword, String usrFirstName, String usrLastName, String usrEmail, UserType usrType, String Status) 
+	public Subscriber(Integer usrId, String usrName, String usrPassword, String usrFirstName, String usrLastName, String usrEmail, UserType usrType, String Status,String phoneNum) 
 	{
 		super(usrName,usrPassword, usrFirstName, usrLastName, usrId, usrEmail,usrType);
 		this.Status=Status;
 		this.subscriberNum= usrId;
+		this.phoneNum=phoneNum;
 	}
 
 	public Integer getSubscriberNum() {
