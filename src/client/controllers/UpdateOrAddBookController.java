0e431c933475098c9636101352955f4174deadfe
נועם דרורי < @DESKTOP-DUMMA12.mynet>
2019-01-24 20:15:56 +0200

    ss

--- src/client/controllers/UpdateOrAddBookController.java
+++ src/client/controllers/UpdateOrAddBookController.java
@@ -136,6 +136,7 @@ public class UpdateOrAddBookController {
     	System.out.println(query);
     	ViewStarter.client.handleMessageFromClientUI(new Message(OperationType.UpdateBookDetails,query ));
 
+    	
     }
     
     public void showSelectedBookDetails(Book book)
